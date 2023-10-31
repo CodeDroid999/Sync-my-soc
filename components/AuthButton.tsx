f5fa571ff38d3faacee1c2ad702f098e3d0aaabb
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-31 13:43:32 +0300

    quick fix

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -11,7 +11,7 @@ export default async function AuthButton() {
   } = await supabase.auth.getUser()
 
   return user ? (
-    <div className="flex items-center gap-4 text-blue-900">
+    <div className="items-center gap-4 text-blue-900 text-right">
       <div className="flex space-x-2">
         <AboutButton/>
         <DashboardButton/>
