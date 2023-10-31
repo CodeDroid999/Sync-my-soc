35fbfcbe5d64f769c62d8cd01222571bfbe781da
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-31 16:54:49 +0300

    remove messages

--- components/DashboardButton.tsx
+++ components/DashboardButton.tsx
@@ -2,7 +2,7 @@ export default function DashboardButton() {
     return (
       <a
         className="text-blue-900 bg-white border-blue-900 py-2 px-3 flex  no-underline hover:bg-blue-900 hover:text-white border"
-        href=""
+        href="/dashboard"
         target="_blank"
         rel="noreferrer"
       >
