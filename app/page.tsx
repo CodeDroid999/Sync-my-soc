7a9abc6a568627d3da7a03b50f23f42faacd74b7
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-31 12:39:04 +0300

    add About button

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,13 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center ">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 bg-white">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
-          <AboutButton/>
-          {isSupabaseConnected && <AuthButton />}
+          <div className="flex space-x-2">
+            <AboutButton />
+            {isSupabaseConnected && <AuthButton />}
+          </div>
         </div>
       </nav>
 
