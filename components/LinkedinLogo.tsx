f10c323ee3af87d4f2eac215366497a561fe2dd4
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-31 16:05:48 +0300

    add linkedin logo

---
+++ components/LinkedinLogo.tsx
@@ -0,0 +1,8 @@
+export default function LinkedinLogo() {
+    return (
+      <div className="">
+        <img src="https://i.postimg.cc/3NRJt2B4/linkedin-sync-my-social.png" height={50} alt=""></img>
+      </div>
+    )
+  }
+  
