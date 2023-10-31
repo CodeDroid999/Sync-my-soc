5deedadc0fd079e74f72d5af96faf02c89495234
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-31 11:03:07 +0300

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient()
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center bg-blue-500">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
