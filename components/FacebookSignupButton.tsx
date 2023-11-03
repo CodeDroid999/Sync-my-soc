890aa457bbb3d11f1f92c7a2d86b1055c139d4a1
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-03 15:11:32 +0300

    fixing

--- components/FacebookSignupButton.tsx
+++ components/FacebookSignupButton.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
 
 function SignUpWithFacebookButton() {
-    return (
-        <Link href="/auth/facebook-sign-in">
-            <button className="bg-blue-100 text-orange-500">Sign Up with Facebook</button>
-        </Link>
-
-    );
+  return (
+    <Link href="/auth/facebook-sign-in">
+      <button className="bg-blue-100 text-orange-500">Sign Up with Facebook</button>
+    </Link>
+  );
 }
 
 export default SignUpWithFacebookButton;
