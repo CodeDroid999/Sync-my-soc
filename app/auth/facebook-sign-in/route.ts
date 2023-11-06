ddbe827fbbc3abb5cdc5faff0a657407cc199f54
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-06 10:20:53 +0300

    export default (req: NextApiRequest, res: NextApiResponse) => {   return NextAuth(req, res, options); };

--- app/auth/facebook-sign-in/route.ts
+++ app/auth/facebook-sign-in/route.ts
@@ -1,3 +1,4 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 
@@ -8,20 +9,8 @@ const options = {
       clientSecret: process.env.FACEBOOK_SECRET,
     }),
   ],
-  // Add other options as needed
 };
 
-export default (req, res) => {
-  try {
-    // Log the request for debugging
-    console.log('Incoming request:', req);
-
-    // Execute NextAuth with the specified options
-    return NextAuth(req, res, options);
-  } catch (error) {
-    // Handle and log errors
-    console.error('Error in Facebook sign-in route:', error);
-    // Return an error response or redirect to an error page
-    res.status(500).json({ error: 'An error occurred during sign-in.' });
-  }
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, options);
 };
