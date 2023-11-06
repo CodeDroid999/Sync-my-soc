b7bb313785fd6d8ac38139462d986bc78fb8475e
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-06 10:40:36 +0300

    resolve route

--- app/auth/facebook-sign-in/route.ts
+++ app/auth/facebook-sign-in/route.ts
@@ -2,12 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 
-export default NextAuth({
+const options = {
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID || process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_SECRET || process.env.FACEBOOK_APP_SECRET,
+      clientId: process.env.FACEBOOK_ID || '',
+      clientSecret: process.env.FACEBOOK_SECRET || '',
     }),
   ],
-  // Other options, callbacks, etc.
-});
+};
