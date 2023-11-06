05e765a164d55b697e9a8103ea8bec8e3d78ad17
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-06 10:18:31 +0300

    fic method export

--- app/auth/facebook-sign-in/route.ts
+++ app/auth/facebook-sign-in/route.ts
@@ -2,12 +2,26 @@ import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 
 const options = {
-    providers: [
-        FacebookProvider({
-          clientId: process.env.FACEBOOK_ID,
-          clientSecret: process.env.FACEBOOK_SECRET
-        }),
-    ],
-}
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+  ],
+  // Add other options as needed
+};
 
-export default (req, res) => NextAuth(req, res, options)
+export default (req, res) => {
+  try {
+    // Log the request for debugging
+    console.log('Incoming request:', req);
+
+    // Execute NextAuth with the specified options
+    return NextAuth(req, res, options);
+  } catch (error) {
+    // Handle and log errors
+    console.error('Error in Facebook sign-in route:', error);
+    // Return an error response or redirect to an error page
+    res.status(500).json({ error: 'An error occurred during sign-in.' });
+  }
+};
