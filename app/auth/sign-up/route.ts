b351603e2c71c8427bdfd9126b57856a63e5ee6a
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-06 10:24:15 +0300

    fix route config

--- app/auth/sign-up/route.ts
+++ app/auth/sign-up/route.ts
@@ -1,41 +1,40 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
-import FacebookProvider from "next-auth/providers/facebook";
+import { createClient } from '@/utils/supabase/server';
+import { NextApiResponse, NextApiRequest } from 'next';
 
-export const dynamic = "force-dynamic";
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const requestUrl = new URL(req.url, 'http://localhost');
+    const formData = new URLSearchParams(await req.text());
+    const email = formData.get('email') || '';
+    const password = formData.get('password') || '';
+    const username = formData.get('username') || ''; // Collect the username from the form
+    const supabase = createClient();
 
-export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
-  const formData = await request.formData();
-  const email = String(formData.get("email"));
-  const password = String(formData.get("password"));
-  const username = String(formData.get("username")); // Collect the username from the form
-  const supabase = createClient();
+    try {
+      const { error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          emailRedirectTo: `${requestUrl.origin}/auth/callback`,
+          data: { username }, // Include the username in the data object
+        },
+      });
 
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${requestUrl.origin}/auth/callback`,
-      data: { username }, // Include the username in the data object
-    },
-  });
-
-  if (error) {
-    return NextResponse.redirect(
-      `${requestUrl.origin}/login?error=Could not authenticate user`,
-      {
-        // A 301 status is required to redirect from a POST to a GET route
-        status: 301,
+      if (error) {
+        res.writeHead(301, {
+          Location: `${requestUrl.origin}/login?error=Could not authenticate user`,
+        });
+        res.end();
+      } else {
+        res.writeHead(301, {
+          Location: `${requestUrl.origin}/login?message=Check email to continue sign-in process`,
+        });
+        res.end();
       }
-    );
-  }
-
-  return NextResponse.redirect(
-    `${requestUrl.origin}/login?message=Check email to continue sign-in process`,
-    {
-      // A 301 status is required to redirect from a POST to a GET route
-      status: 301,
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-  );
-}
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+};
